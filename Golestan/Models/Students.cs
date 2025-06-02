0663c1a623257d2503160a4e0a0f48dec5b16e7c
eesphgos2 <eesphgos2@gmail.com>
2025-06-02 21:26:14 +0330

    feat(Model,Data):Added Student class and relation with user

---
+++ Golestan/Models/Students.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Golestan.Models
+{
+    public class Students
+    {
+        [Key]
+        public int Student_Id { get; set; }
+        public int User_Id { get; set; }
+        public DateTime Enrollment_Date { get; set; }
+        public Users users { get; set; }
+    }
+}
