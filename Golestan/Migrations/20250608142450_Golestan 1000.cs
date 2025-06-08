529d3f2993a7d1cd6fe9d3213bf0baad527097f8
Seyed Ahmad Mousavi <seyedahmadmosavi29@gmail.com>
2025-06-08 18:01:38 +0330

    feat: adding style to the class Detail page

---
+++ Golestan/Migrations/20250608142450_Golestan 1000.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Golestan.Migrations
+{
+    /// <inheritdoc />
+    public partial class Golestan1000 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
