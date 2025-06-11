29642356e4ec9a6609e6c02b64256f77b3d17964
eesphgos2 <eesphgos2@gmail.com>
2025-06-12 02:36:21 +0330

    fix(view fo assign teacher):fixed drop down of teacher

---
+++ Golestan/Migrations/20250611213419_new feat.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Golestan.Migrations
+{
+    /// <inheritdoc />
+    public partial class newfeat : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "instructorname",
+                table: "Instructors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "instructorname",
+                table: "Instructors");
+        }
+    }
+}
